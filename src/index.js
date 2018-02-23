5dda99c7a1fa43a0b2a7068474342e44e8008344
Oleg Nazarov <oleg.iaguzhinskii@gmail.com>
2018-02-23 17:23:28 +0300

    delete babel-transform-es2015-plugin...

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 render(
-    <div>Welcome to New-York)</div>,
-    document.getElementById('root')
+  <div>Welcome to New-York)</div>,
+  document.getElementById('root')
 );
