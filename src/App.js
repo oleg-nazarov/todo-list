4abe69ab584e68b48a05ec98d64dca262cecd312
Oleg Nazarov <oleg.iaguzhinskii@gmail.com>
2018-02-24 16:26:51 +0300

    add row and col into App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const App = () => (
   <div className="container-fluid">
-    <div>You gotta do what you gotta do</div>
+    <div className="row justify-content-center">
+      <div className="col col-sm-10 col-md-8 col-lg-6">
+        <div>You gotta do what you gotta do</div>
+      </div>
+    </div>
   </div>
 );
 
